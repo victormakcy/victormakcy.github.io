c05d7f1fddc06ae91730eeb12f4ca5748b8a7af9
Victor Mak <victormakcy@gmail.com>
2014-03-05 00:13:58 -0500

    projects can be hidden again

--- scripts/show.js
+++ scripts/show.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
   $("#name").hide(0).delay(400).fadeIn(1000);
   $("#message").hide(0).delay(900).fadeIn(1000);
+
   $("#selection-projects").click(function() {
-  	$("#projects").hide(0).delay(200).fadeIn(1000);
+  	if ($("#selection-projects").hasClass("hidden")) {
+  		$("#projects").hide(0).delay(200).fadeIn(1000);
+  		$("#selection-projects").removeClass("hidden");
+  	} else {
+  		$("#projects").hide();
+  		$("#selection-projects").addClass("hidden");
+  	}
   });
 });
